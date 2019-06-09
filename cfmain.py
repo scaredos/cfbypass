dc8dfa1064526ae9d83ca2e3dd2174943b964bd7
ScaredOS <39132876+scaredos@users.noreply.github.com>
2019-06-09 18:47:10 -0400

    Create cfmain.py

---
+++ cfmain.py
@@ -0,0 +1,38 @@
+## CFBypass Beta v1.BETA
+# Coded on 6/9/2019
+# Inspired by CloudSearch.cf
+# Author = SmallDoink
+# Date = 6/9/2019
+# Version = v1.LINUX
+# Imports
+import os
+import socket
+
+# END Imports
+print """
+    __  _____  ____   __ __  ____   ____  _____ _____
+   /  ]|     ||    \ |  |  ||    \ /    |/ ___// ___/
+  /  / |   __||  o  )|  |  ||  o  )  o  (   \_(   \_
+ /  /  |  |_  |     ||  ~  ||   _/|     |\__  |\__  |
+/   \_ |   _] |  O  ||___, ||  |  |  _  |/  \ |/  \ |
+\     ||  |   |     ||     ||  |  |  |  |\    |\    |
+ \____||__|   |_____||____/ |__|  |__|__| \___| \___|
+
+    CFBypass - CloudFlare Bypass - Version v1.BETA
+                Author - SmallDoink#1102
+"""
+i = raw_input("Website Name> ")
+v = raw_input("Verbosity> ")
+if verbosity <= 1:
+    websitename = "i"
+    ip = socket.gethostbyaddr('ssh.'+i)
+    print "IP Behind Cloudflare - "+ip
+    os.exit()
+    # END OF VERBOSITY 1
+else:
+    websitename = "i"
+    ip = socket.gethostbyname('ssh.'+i)
+    print "Hostname and IP behind Cloudflare - "+ip
+    os.exit()
+    # END OF VERBOSITY 2
+os.exit()
