512741e3542937288b6d7de2357bfc01489e9378
ScaredOS <39132876+scaredos@users.noreply.github.com>
2019-06-10 09:35:42 -0400

    Update cfmain.py

--- cfmain.py
+++ cfmain.py
@@ -4,13 +4,31 @@
 # Author = SmallDoink
 # Date = 6/9/2019
 # Version = v1.MAIN
+# Second Version
 # Imports
-import os 
+import os
 import socket
-
 # END Imports
-os.system("clear")
-print """
+os.system('clear')
+class c:
+        black='\033[30m'
+        red='\033[31m'
+        green='\033[32m'
+        orange='\033[33m'
+        blue='\033[34m'
+        purple='\033[35m'
+        cyan='\033[36m'
+        lightgrey='\033[37m'
+        darkgrey='\033[90m'
+        lightred='\033[91m'
+        lightgreen='\033[92m'
+        yellow='\033[93m'
+        lightblue='\033[94m'
+        pink='\033[95m'
+        lightcyan='\033[96m'
+        white='\033[0m'
+
+print c.red +"""
     __  _____  ____   __ __  ____   ____  _____ _____
    /  ]|     ||    \ |  |  ||    \ /    |/ ___// ___/
   /  / |   __||  o  )|  |  ||  o  )  o  (   \_(   \_
@@ -22,24 +40,7 @@ print """
     CFBypass - CloudFlare Bypass - Version v1.MAIN
                 Author - SmallDoink#1102
 """
-i = raw_input("Website Name> ")
-v = raw_input("Verbosity> ")
-if v == 1:
-    websitename = "i"
-    ip = socket.gethostbyaddr('ssh.'+i)
-    print "IP Behind Cloudflare - "+ip
-
-    # END OF VERBOSITY 1
-elif v == 0:
-    websitename = "i"
-    ip = socket.gethostbyaddr('ssh.'+i)
-    print "IP Behind Cloudflare - "+ip
-
-    # END OF VERBOSITY 0
-else:
-    websitename = "i"
-    ip = socket.gethostbyname('ssh.'+i)
-    print "Hostname and IP behind Cloudflare - "+ip
-
-    # END OF VERBOSITY 2
-
+i = raw_input(c.orange + "Website Name> " + c.white)
+websitename = "i"
+ip = socket.gethostbyaddr('ssh.'+i)
+print c.pink + "IP Behind Cloudflare - "+ip
